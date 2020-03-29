f41096bfa456288eeea3a10d4bda8f5e7c8d8ab0
Daniel Rodrigo Quiñones Pinilla <danielqp1@gmail.com>
2020-03-30 01:00:18 +0200

    Added abstraction and test projects

--- QD.EntityFrameworkCore.UnitOfWork/Repository.cs
+++ QD.EntityFrameworkCore.UnitOfWork/Repository.cs
@@ -1,6 +1,8 @@
-﻿namespace QD.EntityFrameworkCore.UnitOfWork
+﻿using QD.EntityFrameworkCore.UnitOfWork.Abstractions;
+
+namespace QD.EntityFrameworkCore.UnitOfWork
 {
-	public class Repository
+	public class Repository : IRepository
 	{
 		public Repository()
 		{
